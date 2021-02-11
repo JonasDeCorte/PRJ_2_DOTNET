4dfea62cc48ff77d1646c6b1d86ace5f46350b7c
Jonas De Corte <jonas.decorte@student.hogent.be>
2021-02-11 20:33:00 +0100

    Added project layout + first classes

---
+++ Models/Domain/Klant.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projecten2.Models.Domain
+{
+    public class Klant : Gebruiker
+    {
+    }
+}
