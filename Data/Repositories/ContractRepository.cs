06956e3b2c7c2ec218381b586294ca7e2273e046
Jonas De Corte <jonas.decorte@student.hogent.be>
2021-02-22 12:01:52 +0100

    added Contract/Gebruiker/Ticket REPO

---
+++ Data/Repositories/ContractRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using projecten2.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projecten2.Data.Repositories
+{
+    public class ContractRepository : IContractRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<Contract> _contracten;
+        public ContractRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _contracten = _context.Contracten;
+        }
+        public void Add(Contract contract)
+        {
+            _contracten.Add(contract);
+        }
+        public IEnumerable<Contract> GetAll()
+        {
+            return _contracten.Include(x => x.Klant).ThenInclude(x => x.Tickets).ToList();
+        }
+
+        public Contract GetByContractNr(int contractNr)
+        {
+            return _contracten.Include(x => x.Klant).ThenInclude(x => x.Tickets).FirstOrDefault(x => x.ContractNr == contractNr);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Data/Repositories/TicketRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using projecten2.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projecten2.Data.Repositories
+{
+    public class TicketRepository : ITicketRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<Ticket> _tickets;
+
+        public TicketRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _tickets = _context.Tickets;
+        }
+        public void Add(Ticket ticket)
+        {
+            _tickets.Add(ticket);
+        }
+
+        public IEnumerable<Ticket> GetAll()
+        {
+            return _tickets.OrderBy(x => x.TicketStatus).Include(x => x.Gebruiker).ToList();
+        }
+
+        public Ticket GetByTicketNr(int ticketNr)
+        {
+            return _tickets.OrderBy(x => x.TicketStatus).Include(x => x.Gebruiker).FirstOrDefault(x => x.TicketNr == ticketNr);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
