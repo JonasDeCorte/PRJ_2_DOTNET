8ac35abc5c37a04620d62256be28eabf0d5dd520
aurelienhs <aurelien.hebersuffrin@student.hogent.be>
2021-03-09 15:29:45 +0100

    Knoppen lopende contracten en actieve tickets af (design moet nog gebeuren)

--- projecten2/Controllers/HomeController.cs
+++ projecten2/Controllers/HomeController.cs
@@ -3,24 +3,36 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using projecten2.filter;
 using projecten2.Models;
+using projecten2.Models.Domain;
 
 namespace projecten2.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ITicketRepository _ticketRepository;
+        private readonly IContractRepository _contractRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ITicketRepository ticketRepository, IContractRepository contractRepository)
         {
             _logger = logger;
+            _ticketRepository = ticketRepository;
+            _contractRepository = contractRepository;
         }
-
+        [ServiceFilter(typeof(KlantFilter))]
+        [Authorize]
         public IActionResult Index()
         {
-            return View();
+            int[] aantal = new int[2];
+            
+            aantal[0] = _contractRepository.GetAll().Count();
+            aantal[1] = _ticketRepository.GetAll().Count();
+            return View(aantal);
         }
 
         public IActionResult Privacy()
