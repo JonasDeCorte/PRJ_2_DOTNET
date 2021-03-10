3b131a4ec2153b3e2e11867f32365ad3cc2d9626
aurelienhs <aurelien.hebersuffrin@student.hogent.be>
2021-03-10 12:01:01 +0100

    Grafiek verbeterd en datums toegevoegd

--- projecten2/ViewComponents/ChartJsViewComponent.cs
+++ projecten2/ViewComponents/ChartJsViewComponent.cs
@@ -2,63 +2,74 @@
 using projecten2.Models.Chart;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System;
+using projecten2.filter;
+using Microsoft.AspNetCore.Authorization;
+using projecten2.Models.Domain;
 
 namespace projecten2.ViewComponents
 {
     [ViewComponent(Name = "chartjs")]
     public class ChartJsViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        
+        //[ServiceFilter(typeof(KlantFilter))]
+       // [Authorize]
+        public IViewComponentResult Invoke(/*Klant klant*/)
         {
             // Ref: https://www.chartjs.org/docs/latest/
-            var chartData = @"
-            {
-                type: 'bar',
-                responsive: true,
-                data:
-                {
-                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                    datasets: [{
-                        label: '# of Votes',
-                        data: [12, 19, 3, 5, 2, 3],
-                        backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                            ],
-                        borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                            ],
-                        borderWidth: 1
-                    }]
-                },
-                options:
-                {
-                    scales:
-                    {
-                        yAxes: [{
-                            ticks:
-                            {
-                                beginAtZero: true
-                            }
-                        }]
-                    }
-                }
-            }";
 
-            var chart = JsonConvert.DeserializeObject<ChartJs>(chartData);
+
+            Klant klant2 = new Klant();
+            string[] dataLabels2 = new string[6];
+            int[] data = { 0, 0, 0, 0, 0, 0 };
+            DateTime date = DateTime.Today;
+            string.Format("dd mm `yy");
+            DateTime date2 = new DateTime();
+            for(int i = 5; i >= 0; i--)
+            {
+                date2 = date.AddDays(-7);
+                dataLabels2[i] = string.Format("{0:dd/MM/yy}", date2) + "-" + string.Format("{0:dd/MM/yy}", date);
+                foreach(Contract c in klant2.Contracten)
+                    foreach(Ticket t in c.Tickets)
+                    {
+                        if (t.AanmaakDatum > date2)
+                            data[i]+=1;
+                    };
+                date = date2.AddDays(-1);
+            }
+            
+            string[] backgroundColor = {
+                        "rgba(255, 99, 132, 0.2)",
+                        "rgba(255, 99, 132, 0.2)",
+                        "rgba(255, 99, 132, 0.2)",
+                        "rgba(255, 99, 132, 0.2)",
+                        "rgba(255, 99, 132, 0.2)",
+                        "rgba(255, 99, 132, 0.2)"
+                        };
+                            
+            Dataset dataset = new Dataset { label = "Actieve tickets", data = data, borderWidth =1,backgroundColor = backgroundColor};
+            Dataset[] datasets = { dataset };
+            
+
+            ChartJs Chart = new ChartJs
+            {
+                type = "line",
+                responsive = true,
+                data = new Models.Chart.Data
+                {
+                    labels = dataLabels2,
+                    datasets =  datasets 
+                    
+                },
+
+            };
+            
+           /* var chart = JsonConvert.DeserializeObject<ChartJs>(chartData);*/
             var chartModel = new ChartJsViewModel
             {
-                Chart = chart,
-                ChartJson = JsonConvert.SerializeObject(chart, new JsonSerializerSettings
+                Chart = Chart,
+                ChartJson = JsonConvert.SerializeObject(Chart, new JsonSerializerSettings
                 {
                     NullValueHandling = NullValueHandling.Ignore,
                     
