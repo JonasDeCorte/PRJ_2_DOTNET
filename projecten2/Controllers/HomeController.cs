2f4a862090d1311a16f92eed28bb19a280207100
aurelienhs <aurelien.hebersuffrin@student.hogent.be>
2021-03-11 13:53:02 +0100

    Blokerings scherm verbeterd en apparte pagina voor grafiek toegevoegd

--- projecten2/Controllers/HomeController.cs
+++ projecten2/Controllers/HomeController.cs
@@ -39,6 +39,12 @@ namespace projecten2.Controllers
         {
             return View();
         }
+        [ServiceFilter(typeof(KlantFilter))]
+        [Authorize]
+        public IActionResult Chart()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
